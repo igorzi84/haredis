be30d728ba7596b98bd203002ca620ed32dc8f70
Igor <igorzi84@gmail.com>
2019-01-11 09:37:53 -0500

    Fixed start.sh with bash

--- sentinel/start.sh
+++ sentinel/start.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 redis-sentinel /etc/sentinel.conf &
-if [ $ROLE == 'slave' ];then 
+if [[ $REDIS_ROLE == 'slave' ]]; then 
     redis-server --slaveof redis-master 6379
 else
     redis-server
